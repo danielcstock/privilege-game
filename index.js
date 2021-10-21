c750531e235da1e127d448a5c3915f18bdd7e5ce
danielstock <danielcstock12@gmail.com>
2021-10-21 13:30:54 -0300

    Correção da porta.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 const app = customExpress();
 
-http.createServer(app).listen(80)
-console.log("Listening at:// port:%s (HTTP)", 80)
+http.createServer(app).listen(3000)
+console.log("Listening at:// port:%s (HTTP)", 3000)
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
