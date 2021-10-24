b4fc11e3df66c971c07bade863d01a02918ebfb3
Andrea Zambrana <andrea.zambrana@unico.io>
2021-10-24 17:31:21 -0300

    feat: add material ui and react router

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,11 +1,48 @@
-import styles from './App.module.css';
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import theme from "./theme";
+
+// const { REACT_APP_TEST_VAR } = process.env;
 
 function App() {
   return (
-    <div className={styles.App}>
-      Jogo dos privilegios
-    </div>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/avatar">Avatar
+            </Link>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/avatar">
+              <Avatar />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </>
   );
+}
+
+function Home() {
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
+  );
+}
+
+function Avatar() {
+  return <>Avatar</>;
 }
 
 export default App;
