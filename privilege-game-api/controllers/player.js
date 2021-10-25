5149c242c8b01ddf6d73a79d5914360059f341fc
danielstock <danielcstock12@gmail.com>
2021-10-25 13:56:09 -0300

    Swagger documentation.

--- privilege-game-api/controllers/player.js
+++ privilege-game-api/controllers/player.js
@@ -4,19 +4,29 @@ const Player = require('../models/player')
 // Exportar a função da controller
 module.exports = app => {
     // Get
-    app.get('/player', (req, res) => {
+    app.get('/player/:id', (req, res) => {
         // #swagger.tags = ['Player']
-        // #swagger.description = 'Endpoint para obter um usuário.'
-        res.send("Get player susccesfully!")
+        // #swagger.description = 'Gets an existing player.'
+        // #swagger.parameters['id'] = { description: 'Player\'s id.' }
+        p = Player.getPlayer(req.params.id);
+        res.send(p);
     });
 
     // Post
     app.post('/player', (req, res) => {
         // #swagger.tags = ['Player']
-        // #swagger.description = 'Endpoint para criar um player.'
-        const player = req.body;
-        Player.createPlayer(player);
+        // #swagger.description = 'Creates a new player.'
+        /* #swagger.parameters['newUser'] = {
+               in: 'body',
+               description: 'Player\'s info.',
+               required: true,
+               type: 'object',
+               schema: { $ref: "#/definitions/addPlayer" }
+        } */
 
-        res.send("Post player succesfully")
+        const player = req.body;
+        const p = Player.createPlayer(player);
+
+        res.send(p)
     });
 }
--- privilege-game-api/models/player.js
+++ privilege-game-api/models/player.js
@@ -1,6 +1,31 @@
 class Player {
     createPlayer(pPlayer) {
+        const p = this.insertPlayer(pPlayer);
         console.log("Player created.")
+        return p;
+    }
+
+    getPlayer(pId){
+        const id = pId;
+        const email = 'daniel.stock@unico.io';
+        const occupation = 'developer';
+        const skin_tone = 5;
+        const gender = 3;
+        return {id, email, occupation, skin_tone, gender};
+    }
+
+
+    insertPlayer(pPlayer){
+        const id = 1;
+        const email = pPlayer.email;
+        const occupation = pPlayer.occupation;
+        const skin_tone = pPlayer.skin_tone;
+        const gender = pPlayer.gender;
+        return {id, email, occupation, skin_tone, gender};
+    }
+
+    selectPlayer(pPlayer){
+
     }
 }
 
