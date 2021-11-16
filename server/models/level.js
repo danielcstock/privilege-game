4edc34752ac63c7e67d35708193474e064978243
danielstock <danielcstock12@gmail.com>
2021-11-16 17:43:18 -0300

    Level within race object.

--- server/models/level.js
+++ server/models/level.js
@@ -8,21 +8,15 @@ class Level {
     getLevel(pId){
         const id = pId;
         const boundary = 30;
-        const name = 5;
-        const race = 1;
-        return { id, boundary, name, race };
+        const name = "Level 1";
+        return { id, boundary, name };
     }
 
     insertLevel(pLevel){
         const id = 1;
         const boundary = pLevel.boundary;
         const name = pLevel.name;
-        const race = pLevel.race;
-        return {id, boundary, name, race};
-    }
-
-    selectLevel(pLevel){
-
+        return {id, boundary, name };
     }
 }
 
