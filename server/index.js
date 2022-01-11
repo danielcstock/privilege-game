105e4378f5ac8fb81bd3890d8f1562752778dc86
stock <danielcstock12@gmail.com>
2022-01-11 06:46:15 -0300

    update server default port.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ const customExpress = require('./config/customExpress');
 const http = require('http');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
-const cors = require('cors');
 const app = customExpress();
 
-http.createServer(app).listen(4000)
+http.createServer(app).listen(8080);
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-console.log("Listening at:// port:%s (HTTP)", 4000)
